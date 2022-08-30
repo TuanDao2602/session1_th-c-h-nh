6ccc115c6c12ec7c9f513561ccde9948bcefa6a6
TuanDao2602 <tuanktktcn@gmail.com>
2022-08-30 08:29:25 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,53 @@
+// let i=10;
+// let f=20.5;
+// let b= true;
+// let s= "hà nội";
+//
+// console.log(i)
+// document.write(f)
+
+
+// bài 2
+// let width = 20
+// let height = 10
+// let dientich = width * height
+// document.write('diện tích = ' + dientich)
+
+
+// bài 3
+// let a=Number(prompt("nhập vào số a"))
+// let b = Number(prompt("nhập vào số b"))
+// if (a%b===0){
+//     alert(" a là bội số của b")
+// }else {
+//     alert(" a không phải là bội của b")
+// }
+
+// bài luyện tập
+// let inputWidth= Number(prompt("nhập vào chiều dài"))
+// let inputHeight=Number(prompt("nhập vào chiều rộng"))
+// let dienTich = inputWidth*inputHeight;
+// console.log(` diện tích là : ${dienTich}`)
+
+
+// bài tập nhập điểm
+// let diemToan =Number(prompt("nhập vào điểm toán"));
+// let diemLy=Number(prompt("nhập vào điểm lý"));
+// let diemSinh=Number(prompt("nhập vào điểm sinh"));
+// let tb = (diemToan+diemSinh+diemLy)/3;
+// alert(`điểm tb là : ${tb}`)
+
+
+// BÀI TẬP ĐỘ c>f
+// let doC= Number(prompt("nhập vào độ C"))
+// let doF= (doC*1.8) + 32
+// alert(`ĐỘ f LÀ : ${doF}`)
+
+//DIỆN TÍCH VÀ CHU VI HÌNH TRÒN
+// let r = Number(prompt("nhập vào bán kính đường tròn"))
+// let  chuVi = (2*3.14)*r;
+// let dienTich = 3.14 *(r*r)
+// alert(`diện tích là ${dienTich} , chu vi là : ${chuVi}`)
+
+let input = document.getElementById("input")
+console.log(input.innerText)
